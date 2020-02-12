020764d22dfc7a9fdb4b9de672ae1fcfd7cf8986
Annaguzel <you@example.com>
2020-02-12 14:07:40 +0200

    Task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,11 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    "my_list":[
+        {"restaurant_name":"Chappati","food_type":"Indian"},
+        {"restaurant_name":"Ocean","food_type":"SeaFood"},
+        {"restaurant_name":"MADO","food_type":"Turkish Food"},
+    ]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +19,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object":{
+    "restaurant_name":"Alia",
+    "food_type":"Arabic",
+    },
 
     }
     return render(request, 'detail.html', context)
